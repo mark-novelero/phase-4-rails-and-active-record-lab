4093a7c8ab92ad68bb7c94bd4f63cb1e8e7ef54a
mark novelero <mark.novelero@gmail.com>
2021-06-07 22:40:22 -0400

    commit tree 874e75c1957b5cf11c8a1d99c3f8ac43fac5f06d

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Student.create(first_name: "Dwayne", last_name: "Johnson", grade: 8)
+
+puts "done"
